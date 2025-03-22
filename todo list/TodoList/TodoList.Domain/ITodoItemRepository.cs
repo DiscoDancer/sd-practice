6fb503fcf91c7225b858be551a6e8437cebac532
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2025-03-22 13:06:38 +0100

    todo list domain

---
+++ todo list/TodoList/TodoList.Domain/ITodoItemRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoList.Domain;
+
+public interface ITodoItemRepository
+{
+    Task<TodoItem> GetAsync(long id);
+    Task<IReadOnlyCollection<TodoItem>> GetAllAsync();
+    Task<bool> AddAsync(TodoItem item);
+    Task<bool> UpdateAsync(TodoItem item);
+    Task<bool> DeleteAsync(long id);
+}
---
+++ todo list/TodoList/TodoList.Domain/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoList.Domain;
+
+public sealed class TodoItem
+{
+    public required long Id { get; init; }
+    public required string Title { get; init; }
+    public required bool IsDone { get; init; }
+    public required DateTime CreatedAt { get; init; }
+}
