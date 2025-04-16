a1b81b76c62cb160ad40f6c282d2b958be6ca3e7
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2025-04-16 08:02:23 +0200

    +1

--- todo list/TodoList/TodoList.Domain/TodoItem.cs
+++ todo list/TodoList/TodoList.Domain/TodoItem.cs
@@ -7,9 +7,25 @@ public sealed class TodoItem
     public required bool IsDone { get; init; }
     public required DateTime CreatedAt { get; init; }
 
-
     public override string ToString()
     {
-        return $"Id: {Id}, Title: {Title}, IsDone: {IsDone}, CreatedAt: {CreatedAt}";
+        return $"Id: {Id}, Title: {Title}, IsDone: {IsDone}, CreatedAt: {CreatedAt:O}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is TodoItem other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Title, IsDone, CreatedAt);
+    }
+    private bool Equals(TodoItem other)
+    {
+        return Id == other.Id
+               && string.Equals(Title, other.Title, StringComparison.Ordinal)
+               && IsDone == other.IsDone
+               && CreatedAt == other.CreatedAt;
     }
 }
